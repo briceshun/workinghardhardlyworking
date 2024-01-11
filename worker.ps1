51492bb62a4df852324466e11818cf2dfcefa73d
Brice Shun <37715091+briceshun@users.noreply.github.com>
2024-01-12 00:57:51 +1300

    Indefinite wake mod

--- worker.ps1
+++ worker.ps1
@@ -6,9 +6,6 @@ Machine won't remain awake after $work_end time
 Clear-Host
 $WShell = New-Object -com "Wscript.Shell"
 $num_loops = 0
-$indefinite = $true
-$work_start = Get-Date '09:00'
-$work_end = Get-Date '17:00'
 $interval = 175
 
 <#
@@ -18,33 +15,17 @@ Infinite loop for sleep prevention
 while ($true) {
     $num_loops++
     $nl_str = $num_loops.ToString("000")
-    $now = Get-Date
-    
+    $dt = Get-Date -Format "yyyy-MM-dd HH:mm:ss K"
+
     <#
     .INDEFINITEAWAKE
     Keeps machine PERMANENTLY awake by toggling SCROLLOCK
     #>
-    if ($indefinite -eq $true) {
-        Echo "Loop $nl_str, $now, Wake - Indefinite"
-        $Wshell.sendkeys("{SCROLLLOCK}")
-        Start-Sleep -Milliseconds 100
-        $Wshell.sendkeys("{SCROLLLOCK}")
-    }
-    <#
-    .TIMEAWAKE
-    Keeps machine awake by toggling SCROLLOCK during specified interval
-    #>
-    else {
-        if ($now.TimeOfDay -ge $work_start.TimeOfDay -and $now.TimeOfDay -le $work_end.TimeOfDay){
-            Echo "Loop $nl_str, $now, Wake"
-            $Wshell.sendkeys("{SCROLLLOCK}")
-            Start-Sleep -Milliseconds 100
-            $Wshell.sendkeys("{SCROLLLOCK}")
-        }
-        else {
-            Echo "Loop $nl_str, $now, Sleep"
-        }
-    }
+    Echo "Loop $nl_str, $dt"
+    $Wshell.sendkeys("{SCROLLLOCK}")
+    Start-Sleep -Milliseconds 100
+    $Wshell.sendkeys("{SCROLLLOCK}")
+    
     <#
     .MINIMISE
     Window will minimise to reduce distraction
